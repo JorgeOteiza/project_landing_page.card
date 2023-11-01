44ccfa5531376b48f9f8b50a30a25f757b5c8586
JorgeOteiza <oteiza.jor@gmail.com>
2023-10-31 22:51:47 -0300

    footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Footer = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        setIsVisible(scrollY > 0);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="footer bg-black color-white fixed-bottom">
+        <div className={`footer bg-black color-white ${isVisible ? "visible" : "hidden"}`}>
             <p>Copyright @ Your Website 2023</p>
         </div>
     );
 };
+const handleScroll = () => {
+    const scrollY = window.scrollY;
+    setIsVisible(scrollY === 0);
+};
+
 
